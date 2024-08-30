bb709143386ac9d92c1b3da05bc82a6e7195d310
KD124 <s3836280@rmit.edu.vn>
2024-08-30 13:42:09 +0700

    add functions

---
+++ Functions.sql
@@ -0,0 +1,17 @@
+drop function if exists getStaffSal;
+
+DELIMITER $$
+CREATE FUNCTION getStaffSal(StaffID INT)
+RETURNS INT NOT DETERMINISTIC
+READS SQL DATA
+BEGIN
+	declare sal int;
+    
+    select Salary into sal
+    from Staff s where s.StaffID = StaffID;
+	
+    return sal;
+END $$
+
+
+DELIMITER ;
