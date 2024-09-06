d0824b40631fb5ce5773a0dad9f78ae1a7ea5282
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-09-06 16:38:23 +0700

    fix date format patientTreatmentReport

--- CreateMVs.sql
+++ CreateMVs.sql
@@ -7,9 +7,7 @@ SELECT
     concat(p.FirstName, ' ', p.LastName) AS PatientName,
     concat(s.FirstName, ' ', s.LastName) AS DoctorName,
     t.TreatmentID, 
-    t.StartDate,
-    t.EndDate,
-    CURDATE() AS ReportDate
+    concat(DATE_FORMAT(t.StartDate, '%b %d, %Y'),' - ', DATE_FORMAT(t.EndDate, '%b %d, %Y')) AS Date
 FROM 
     TreatmentHistory t
 JOIN patients p 
@@ -48,3 +46,38 @@ JOIN
 GROUP BY 
     s.StaffID;
 ALTER TABLE StaffPerformanceReport ADD COLUMN ReportID INT AUTO_INCREMENT PRIMARY KEY;
+
+
+-- Billing MV
+-- CREATE TABLE Billing AS
+-- SELECT 
+--     concat(p.FirstName, ' ', p.LastName) AS PatientName,
+--     concat(s.FirstName, ' ', s.LastName) AS DoctorName,
+--     t.TreatmentID, 
+--     t.StartDate,
+--     t.EndDate,
+--     CURDATE() AS ReportDate
+-- FROM 
+--     TreatmentHistory t
+-- JOIN patients p 
+-- ON t.PatientID = p.PatientID
+-- JOIN staff s
+-- ON t.DoctorID = s.StaffID
+-- WHERE 
+--     t.StartDate >= '2024-01-01' AND t.EndDate <= '2024-12-31';
+-- ALTER TABLE PatientTreatmentReport ADD COLUMN ReportID INT AUTO_INCREMENT PRIMARY KEY;
+
+-- CREATE TABLE StaffWorkloadReport AS
+-- SELECT 
+--     s.StaffID, 
+--     COUNT(t.TreatmentID) AS TotalTreatments, 
+--     SUM(TIMESTAMPDIFF(HOUR, t.StartDate, t.EndDate)) AS TotalWorkloadHours, 
+--     CURDATE() AS ReportDate
+-- FROM 
+--     Staff s
+-- JOIN 
+--     TreatmentHistory t ON s.StaffID = t.DoctorID
+-- GROUP BY 
+--     s.StaffID;
+-- ALTER TABLE StaffWorkloadReport ADD COLUMN ReportID INT AUTO_INCREMENT PRIMARY KEY;
+-- 	
